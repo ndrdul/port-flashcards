b839f1530f309816c1320415acadf62c2f44ef3a
ndrdul <devDrdul@protonmail.com>
2022-05-31 19:17:06 -0400

    grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./app.css";
 
 import SAMPLE_FLASHCARDS from "./data/portData";
@@ -8,9 +8,14 @@ import Card from "./components/Card";
 
 const App = () => {
   return (
-    <div>
-      <Card data={portInfoData} buttonTitle={"Random Protocol"} />
-      <Card data={SAMPLE_FLASHCARDS} buttonTitle={"Random Port"} />
+    <div className="grid-container">
+      <div className="grid-item">
+        <Card data={portInfoData} buttonTitle={"Random Protocol"} />
+      </div>
+
+      <div className="grid-item">
+        <Card data={SAMPLE_FLASHCARDS} buttonTitle={"Random Port"} />
+      </div>
     </div>
   );
 };
