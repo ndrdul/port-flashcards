9b75baa196d86a2b5439a72522a32d113dbbf60e
ndrdul <devDrdul@protonmail.com>
2022-05-31 14:09:42 -0400

    port data

--- src/App.js
+++ src/App.js
@@ -11,16 +11,140 @@ const App = () => {
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
-    question: "SNMP",
-    answer: 'Port 161',
-    options: ["2", "3", "4", "5"],
+    question: "FTP",
+    answer: "Port 20 and 21",
   },
   {
     id: 2,
-    question: "Question 2",
-    answer: "answer 2",
-    options: ["2", "3", "4", "5"],
+    question: "SSH",
+    answer: "Port 22",
   },
+  {
+    id: 3,
+    question: "SFTP",
+    answer: "Port 22",
+  },
+  {
+    id: 4,
+    question: "Telnet",
+    answer: "Port 23",
+  },
+  {
+    id: 5,
+    question: "SMTP",
+    answer: "Port 25",
+  },
+  {
+    id: 6,
+    question: "DNS",
+    answer: "Port 25",
+  },
+  {
+    id: 7,
+    question: "DHCP",
+    answer: "Port 67 and 68",
+  },
+  {
+    id: 8,
+    question: "TFTP",
+    answer: "Port 69",
+  },
+  {
+    id: 9,
+    question: "HTTP",
+    answer: "Port 80",
+  },
+  {
+    id: 10,
+    question: "POP3",
+    answer: "Port 110",
+  },
+  {
+    id: 11,
+    question: "NTP",
+    answer: "Port 123",
+  },
+  {
+    id: 12,
+    question: "NetBIOS",
+    answer: "Port 139",
+  },
+  {
+    id: 13,
+    question: "IMAP",
+    answer: "Port 143",
+  },
+  {
+    id: 14,
+    question: "SNMP",
+    answer: "Port 161 and 162",
+  },
+  {
+    id: 15,
+    question: "LDAP",
+    answer: "Port 389",
+  },
+  {
+    id: 16,
+    question: "HTTPS",
+    answer: "Port 443",
+  },
+  {
+    id: 17,
+    question: "SMB",
+    answer: "Port 445",
+  },
+  {
+    id: 18,
+    question: "Syslog",
+    answer: "Port 514",
+  },
+  {
+    id: 19,
+    question: "SMTP TLS",
+    answer: "Port 587",
+  },
+  {
+    id: 20,
+    question: "LDAPS",
+    answer: "Port 636",
+  },
+  {
+    id: 21,
+    question: "IMAP SSL",
+    answer: "Port 993",
+  },
+  {
+    id: 22,
+    question: "POP3 SSL",
+    answer: "Port 995",
+  },
+  {
+    id: 23,
+    question: "SQL",
+    answer: "Port 1433",
+  },
+  {
+    id: 24,
+    question: "SQLnet",
+    answer: "Port 1521",
+  },
+  {
+    id: 25,
+    question: "MySQL",
+    answer: "Port 3306",
+  },
+  {
+    id: 26,
+    question: "RDP",
+    answer: "Port 3389",
+  },
+  {
+    id: 27,
+    question: "SIP",
+    answer: "Port 5060 and 5061",
+  },
+
 ];
 
 export default App;
